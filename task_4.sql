cc8483fb1edb902235111f7c8cd39fc5afeaff18
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-28 22:24:05 +0530

    3 done

--- task_4.sql
+++ task_4.sql
@@ -13,5 +13,15 @@ join sales as t2
 on t1.EmployeeID = t2.SalesPersonID
 group by SalesPersonID,t1.FirstName
 order by sold desc
-limit 1
+limit 1;
+
+
+-- List all customers who have made more than 10 purchases.
+select t1.CustomerID,t1.FirstName,count(*) as 'purchases' from customers as t1
+join sales as t2
+on t1.CustomerID = t2.CustomerID
+group by t1.CustomerID, t1.FirstName
+having purchases >10
+order by purchases desc
+
 
